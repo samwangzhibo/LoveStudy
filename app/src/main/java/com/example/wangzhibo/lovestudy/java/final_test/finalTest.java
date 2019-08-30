bb1df15bcbcfc9301428c481cc429acba655101f
wangzhibo03 <wangzhibo03@kuaishou.com>
2019-08-30 15:07:02 +0800

    添加备注

--- app/src/main/java/com/example/wangzhibo/lovestudy/java/final_test/finalTest.java
+++ app/src/main/java/com/example/wangzhibo/lovestudy/java/final_test/finalTest.java
@@ -9,6 +9,7 @@ package com.example.wangzhibo.lovestudy.java.final_test;
 
 public class finalTest {
     public static void main(String[] args) {
+      // 备注
         Entry entry = new Entry("aaa");
 //        modifyEntry(entry);
         modifyEntryFinal(entry);
